8fb18bc291b9989878897481506999d2b10163d7
Andrew Hlynskyi <ahlincq@gmail.com>
2022-06-20 23:36:10 +0300

    Fetch more info

--- nft-detection-rs/src/bin/nft-detection.rs
+++ nft-detection-rs/src/bin/nft-detection.rs
@@ -2,8 +2,16 @@
 #![allow(unused_variables)]
 
 use solana_client::{rpc_client::RpcClient, rpc_request::TokenAccountsFilter};
-use solana_sdk::{commitment_config::CommitmentConfig, pubkey::Pubkey, program_pack::Pack};
-use std::{str::FromStr, time::Duration};
+use solana_sdk::{
+    commitment_config::CommitmentConfig,
+    program_pack::{IsInitialized, Pack},
+    pubkey::Pubkey,
+};
+use anyhow::{anyhow, Result};
+use mpl_token_metadata::deser::meta_deser;
+use mpl_token_metadata::pda::{find_master_edition_account, find_metadata_account};
+use mpl_token_metadata::state::{Key, MasterEditionV2, Metadata, MAX_MASTER_EDITION_LEN};
+use std::{fmt::Debug, str::FromStr, time::Duration};
 
 const MAINNET_ENDPOINT: &str = "https://api.mainnet-beta.solana.com";
 const DEVNET_ENDPOINT: &str = "https://api.devnet.solana.com";
@@ -38,16 +46,61 @@ fn main() {
 
     println!("{}, {result:#?}", result.len());
 
+    // let result = client.get_account(&owner).unwrap();
+    // println!("{result:?}");
+
     let token1 = Pubkey::from_str("Emb4Td3VrhsYa4jE5jMNsa8FYibWb81Y8VcKotKHibzZ").unwrap();
+    let mint = "7jHNVMSB6X8NgjFHGKSQCxp6AoMycFK2rNyWCjw8E2yp";
+
     let result = client.get_token_account(&token1).unwrap();
     println!("{result:#?}");
 
-    let data = client.get_account_data(&token1).unwrap();
+    fetch_account::<spl_token::state::Account>(&client, token1.to_string().as_str());
+    fetch_account::<spl_token::state::Mint>(&client, mint);
+    fetch_account_data(&client, "8oozoJnyB5xbrYK6tiWcgUbK2q4Eyn6v3QA9s2uTWyJE");
+}
+
+fn fetch_account<T>(client: &RpcClient, key: &str)
+where
+    T: Pack + IsInitialized + Debug + 'static,
+{
+    let data = fetch_account_data(client, key);
+    let account = T::unpack(&data).unwrap();
+    println!("{}: {account:#?}", std::any::type_name::<T>());
+}
+
+fn fetch_account_data(client: &RpcClient, key: &str) -> Vec<u8> {
+    let pubkey = Pubkey::from_str(key).unwrap();
+    println!("key: {pubkey}");
+    // let result = client.get_token_account(&pubkey).unwrap();
+    // println!("{result:#?}");
+
+    let data = client.get_account_data(&pubkey).unwrap();
     println!("data: {} {data:?}", data.len());
-    let account = spl_token::state::Account::unpack(&data).unwrap();
-    println!("account: {account:#?}");
+    data
+}
 
-    // let result = client.get_account(&owner).unwrap();
+pub fn find_metadata_pda(mint: &Pubkey) -> Pubkey {
+    let (pda, _bump) = find_metadata_account(mint);
 
-    // println!("{result:?}");
+    pda
+}
+
+pub type PdaInfo<T> = (Pubkey, T);
+
+pub fn get_metadata_pda(mint: &Pubkey, client: &RpcClient) -> Result<PdaInfo<Metadata>> {
+    let metadata_pubkey = find_metadata_pda(mint);
+    let metadata_account = client.get_account(&metadata_pubkey).map_err(|_| {
+        anyhow!(
+            "Couldn't find metadata account: {}",
+            &metadata_pubkey.to_string()
+        )
+    })?;
+    let metadata = meta_deser(&mut metadata_account.data.as_slice());
+    metadata.map(|m| (metadata_pubkey, m)).map_err(|_| {
+        anyhow!(
+            "Failed to deserialize metadata account: {}",
+            &metadata_pubkey.to_string()
+        )
+    })
 }
